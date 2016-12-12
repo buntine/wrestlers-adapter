46ef1d056ef6254e3d38add503e8b014d82cbe65
Bunts Thy Unholy <bunts@hhd.com.au>
2016-12-12 16:15:08 +1100

    Added struct for LogEntry

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+struct LogEntry<'a> {
+    value: &'a str,
+}
+
+impl<'a> LogEntry<'a> {
+    fn new(v: &str) -> LogEntry {
+        LogEntry{value: v}
+    }
+
+    fn parse_mac_address(self) -> Result<&'a str, &'static str> {
+        Ok("abc123")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Run TCP server as daemon on <PORT>
+    // Take in string
+    // Break into chunks
+    // Get MAC address
+    // POST request to https://wrestlers.hhd.com.au/<MAC>
+    //
+    // Also setup to run on bootup
+
+    let le = LogEntry::new("test test");
+    println!("{}", le.parse_mac_address().unwrap());
 }
