1dcd5407d2f5833f71c3ce27708289f51be4e757
Bunts Thy Unholy <bunts@hhd.com.au>
2016-12-13 13:16:31 +1100

    Adding regex

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+#[macro_use] extern crate lazy_static;
+extern crate regex;
+
+use regex::Regex;
+
 #[derive(Debug, Copy, Clone)]
 pub struct LogEntry<'a> {
     value: &'a str,
@@ -9,7 +14,17 @@ impl<'a> LogEntry<'a> {
     }
 
     fn parse_mac_address(self) -> Result<&'a str, &'static str> {
-        Ok("abc123")
+        lazy_static! {
+            static ref mac: Regex = Regex::new(r"^(:xdigit:{2}::xdigit:{2}::xdigit:{2}::xdigit:{2}::xdigit:{2}::xdigit:{2})$").unwrap();
+        }
+
+        let mut cap = mac.captures_iter(&self.value);
+        let first = match cap.nth(0) {
+            Some(c) => c,
+            None => return Err("No mac address present"),
+        };
+
+        Ok(first.at(0).unwrap())
     }
 
     fn forward(self, mac_address: &'a str, host: &'a str) -> Result<i32, i32> {
@@ -40,7 +55,7 @@ mod tests {
 
     #[test]
     fn parse_mac_address() {
-        let le = LogEntry::new("DATA 127.0.0.1: <14>Dec 12 15:15:20 59-100-240-126.mel.static-ipl.aapt.com.au (\"U7LT,802aa843ac1d,v3.7.21.5389 libubnt[1441]: wevent.ubnt_custom_event(): EVENT_STA_JOIN ath0: 00:34:da:58:8d:a6 / 3")
+        let le = LogEntry::new("DATA 127.0.0.1: <14>Dec 12 15:15:20 59-100-240-126.mel.static-ipl.aapt.com.au (\"U7LT,802aa843ac1d,v3.7.21.5389 libubnt[1441]: wevent.ubnt_custom_event(): EVENT_STA_JOIN ath0: 00:34:da:58:8d:a6 / 3");
 
         assert_eq!(le.parse_mac_address().unwrap(), "00:34:da:58:8d:a6");
     }
