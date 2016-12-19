32efef1e7931ba6c8d23fdef5eed2a7b05b7b5fc
Bunts Thy Unholy <bunts@hhd.com.au>
2016-12-19 17:40:28 +1100

    Reverted to looping over blocking read because closing the socket did not work correctly

--- src/main.rs
+++ src/main.rs
@@ -79,27 +79,36 @@ impl<'a> LogEntry<'a> {
     }
 }
 
-fn handle_stream(mut s: TcpStream, forward_socket: &str) -> Result<String, String> {
-    let mut read = [0; 1028];
+fn handle_stream(mut s: TcpStream, forward_socket: &str) {
+    loop {
+        let mut read = [0; 1028];
 
-    match s.read(&mut read) {
-        Ok(n) => {
-            if n == 0 { 
-                return Err("Connection closed early".to_string());
+        match s.read(&mut read) {
+            Ok(n) => {
+                if n == 0 { 
+                    error!("Connection closed");
+                }
+
+                let log_details = String::from_utf8_lossy(&read[0..n]);
+                let le = LogEntry::new(&log_details[..]);
+
+                let action = match le.parse_action() {
+                    Ok(a) => a,
+                    Err(e) => {
+                        error!("Failed: {}", e);
+                        return;
+                    },
+                };
+
+                match le.forward(&action, forward_socket) {
+                    Ok(_) => info!("Sent {:?}", action),
+                    Err(_) => error!("Failed {:?}", action),
+                }
+            },
+            Err(e) => {
+                error!("Cannot read from socket: {}", e);
+                return;
             }
-
-            let log_details = String::from_utf8_lossy(&read[0..n]);
-            let le = LogEntry::new(&log_details[..]);
-
-            let action = le.parse_action()?;
-
-            return match le.forward(&action, forward_socket) {
-                Ok(_) => Ok(format!("Sent: {:?}", action)),
-                Err(_) => Err(format!("Failed: {:?}", action)),
-            };
-        },
-        Err(e) => {
-            return Err(format!("Invalid stream: {:?}", e));
         }
     }
 }
@@ -134,7 +143,7 @@ fn main() {
     env_logger::init().expect("Cannot open log");
     info!("Opened log");
 
-    daemonize(&listen_socket[..]);
+    //daemonize(&listen_socket[..]);
 
     for conn in listener.incoming() {
         match conn {
@@ -142,108 +151,10 @@ fn main() {
                 let forward_socket = shared_forward_socket.clone();
 
                 thread::spawn(move || {
-                    match handle_stream(s, &forward_socket[..]) {
-                        Ok(s) => info!("{}", s),
-                        Err(s) => warn!("{}", s),
-                    }
+                    handle_stream(s, &forward_socket[..]);
                 });
             },
             Err(_) => continue,
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use hyper::status::StatusCode;
-
-    #[test]
-    fn new_log_entry() {
-        let le = LogEntry::new("test");
-
-        assert_eq!(le.value, "test");
-    }
-
-    #[test]
-    fn parse_action_die() {
-        let le = LogEntry::new("this is invalid");
-
-        assert!(le.parse_action().is_err());
-        assert_eq!(le.parse_action(), Err("Invalid log entry format"));
-    }
-
-    #[test]
-    fn parse_action1() {
-        let le = LogEntry::new("DATA 127.0.0.1: <14>Dec 12 15:15:20 10.our.host.com.au (\"YO,v3.7.21.5389 libubnt[1441]: wevent.cust(): EVENT_STA_JOIN ath0: 00:34:da:58:9d:a7 / 3");
-
-        assert!(le.parse_action().is_ok());
-        assert_eq!(le.parse_action().unwrap(), Action::Join("00:34:da:58:9d:a7"));
-    }
-
-    #[test]
-    fn parse_action2() {
-        let le = LogEntry::new("DATA 127.0.0.1: <14>Dec 12 15:15:20 10.our.host.com.au (\"YO,v3.7.21.5389 libubnt[1441]: wevent.cust(): EVENT_STA_JOIN ath0: 0a:99:da:ab:19:c6 / 3");
-
-        assert!(le.parse_action().is_ok());
-        assert_eq!(le.parse_action().unwrap(), Action::Join("0a:99:da:ab:19:c6"));
-    }
- 
-    #[test]
-    fn parse_action3() {
-        let le = LogEntry::new("[1441]: wevent.ubnt(): EVENT_STA_LEAVE ath0: 5a:98:da:ab:19:c6 / 3");
-
-        assert!(le.parse_action().is_ok());
-        assert_eq!(le.parse_action().unwrap(), Action::Leave("5a:98:da:ab:19:c6"));
-    }
-
-    #[test]
-    fn forward_die() {
-        let le = LogEntry::new("[1441]: EVENT_JOIN wevent.ubnt(): ath0: 5a:98:da:ab:19:c6 / 3");
-        let action = le.parse_action();
-
-        assert!(action.is_ok());
-
-        let res = le.forward(&action.unwrap(), "doesnotexist.hhd.com.au");
-
-        assert!(res.is_err());
-        assert_eq!(res, Err(StatusCode::ServiceUnavailable));
-    }
-
-    #[test]
-    fn forward_ok() {
-        let le = LogEntry::new("DATA 127.0.0.1: <14>Dec 12 15:15:20 10.our.host.com.au (\"YO,v3.7.21.5389 libubnt[1441]: wevent.cust(): EVENT_STA_JOIN ath0: 00:34:da:58:8d:a6 / 3");
-        let action = le.parse_action();
-
-        assert!(action.is_ok());
-
-        let res = le.forward(&action.unwrap(), "wrestlers.hhd.com.au");
-
-        assert!(res.is_ok());
-        assert_eq!(res, Err(StatusCode::Ok));
-    }
-
-    #[test]
-    fn action_from_str_ok() {
-        let j = Action::from_str("JOIN", "123");
-        assert_eq!(j, Ok(Action::Join("123")));
-
-        let l = Action::from_str("LEAVE", "123");
-        assert_eq!(l, Ok(Action::Leave("123")));
-    }
-
-    #[test]
-    fn action_from_str_die() {
-        let j = Action::from_str("UNKNOWN", "123");
-        assert_eq!(j, Err("Invalid action"));
-    }
-
-    #[test]
-    fn action_to_url() {
-        let j = Action::from_str("JOIN", "123").unwrap();
-        assert_eq!(j.to_url("test.com"), "http://test.com/join/123");
-
-        let l = Action::from_str("LEAVE", "12:32:45:65:aa:ff").unwrap();
-        assert_eq!(l.to_url("tester.com"), "http://tester.com/leave/12:32:45:65:aa:ff");
     }
 }
